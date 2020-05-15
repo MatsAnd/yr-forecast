413a3006ee8bffa62642e37a01380ad56c638b10
Mats Andreassen <matsand@live.no>
2020-05-15 19:49:52 +0200

    Export module

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  getForecast: require('./lib/get-forecast'),
+  getNowcast: require('./lib/get-nowcast'),
+  getTextForecast: require('./lib/get-text-forecast'),
+  getCurrentAreas: require('./lib/get-current-areas')
+}
